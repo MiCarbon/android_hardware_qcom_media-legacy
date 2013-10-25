75b983b667a5052766792e24f27ba680ed1caa47
Peter Cai <xqsx43cxy@126.com>
2013-10-25 20:41:13 +0800

    media-legacy: support display-legacy-caf usage
    display-legacy-caf does not have its own media

--- Android.mk
+++ Android.mk
@@ -3,3 +3,10 @@ ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
 include $(call all-subdir-makefiles)
 
 endif
+
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy-caf)
+
+include $(call all-subdir-makefiles)
+
+endif
+
